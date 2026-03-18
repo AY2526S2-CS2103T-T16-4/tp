ff753ea60eb1a4d26dc5f27885e3c57831dc3d78
Chang Jia Jun <135327869+jiajunchang2002g@users.noreply.github.com>
2026-03-18 23:43:26 +0800

    Fix style

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -27,7 +27,6 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
  * Contains helper methods for testing commands.
  */
 public class CommandTestUtil {
-        
     public static final String VALID_NAME_AMY = "Amy Bee";
     public static final String VALID_NAME_BOB = "Bob Choo";
     public static final String VALID_ALIAS_AMY = "Agent Amy";
